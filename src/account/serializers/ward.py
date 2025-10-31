e3754a4b1a403ed13e0ec93d1b239eae2487a73a
phamquiduong <phamquiduong@outlook.com>
2025-10-31 08:55:35 +0700

    Create location API

--- src/account/serializers/ward.py
+++ src/account/serializers/ward.py
@@ -4,6 +4,8 @@ from account.models import Ward
 
 
 class WardSerializer(serializers.ModelSerializer):
+    province = serializers.HyperlinkedRelatedField(view_name='account_provinces_api-detail', read_only=True)
+
     class Meta:
         model = Ward
         fields = '__all__'
--- src/account/urls.py
+++ src/account/urls.py
@@ -2,16 +2,19 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 from account.views.api.auth import LoginAPIView, LogoutAPIView, RefreshTokenAPIView, VerifyTokenAPIView
+from account.views.api.location import ProvinceViewSet, WardViewSet
 from account.views.api.user import UserViewSet
 
 router = DefaultRouter()
-router.register('users', UserViewSet, basename='users_api')
+router.register('users', UserViewSet, basename='account_users_api')
+router.register('provinces', ProvinceViewSet, basename='account_provinces_api')
+router.register('wards', WardViewSet, basename='account_wards_api')
 
 api = [
-    path('login', LoginAPIView.as_view(), name='auth_login_api'),
-    path('refresh', RefreshTokenAPIView.as_view(), name='auth_refresh_api'),
-    path('verify', VerifyTokenAPIView.as_view(), name='auth_verify_api'),
-    path('logout', LogoutAPIView.as_view(), name='auth_logout_api'),
+    path('login', LoginAPIView.as_view(), name='account_login_api'),
+    path('refresh', RefreshTokenAPIView.as_view(), name='account_refresh_api'),
+    path('verify', VerifyTokenAPIView.as_view(), name='account_verify_api'),
+    path('logout', LogoutAPIView.as_view(), name='account_logout_api'),
 
     path('', include(router.urls)),
 ]
